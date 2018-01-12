8ccbc6cb0f7c0611e60b08845dff0e1f61f9040b
BabitsPaul <phi.u.934@gmail.com>
2018-01-12 18:40:55 +0100

    stubs implemented

---
+++ src/Polynomial.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class PolynomialImpl
+	implements Polynomial
+{
+	private ArrayList<Long> coefficients;
+
+	public PolynomialImpl(ArrayList<Long> coefficients)
+	{
+		this.coefficients = coefficients;
+	}
+
+	@Override
+	public void add(Polynomial p) {
+
+	}
+
+	@Override
+	public void sub(Polynomial p) {
+
+	}
+
+	@Override
+	public void mul(Polynomial p) {
+
+	}
+
+	@Override
+	public void div(Polynomial p) {
+
+	}
+
+	@Override
+	public void mod(Polynomial p) {
+
+	}
+
+	@Override
+	public void mod(long p) {
+
+	}
+
+	public static PolynomialImpl parse(String s)
+	{
+		return null;
+	}
+}
